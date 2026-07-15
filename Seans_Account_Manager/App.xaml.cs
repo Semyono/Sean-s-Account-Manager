b6f79502d73818f364f4af599ab5323fe2dc10c6
Semyono <semyonma246@gmail.com>
2026-07-15 12:19:48 +0300

    idk

--- Seans_Account_Manager/App.xaml.cs
+++ Seans_Account_Manager/App.xaml.cs
@@ -1,5 +1,3 @@
-using System.IO;
-using System.Reflection;
 using System.Windows;
 using Seans_Account_Manager.Services;
 
@@ -10,37 +8,7 @@ public partial class App : Application
     protected override void OnStartup(StartupEventArgs e)
     {
         base.OnStartup(e);
-        ExtractEmbeddedNativeDlls();
         MultiRobloxService.Enable();
-    }
-
-    private void ExtractEmbeddedNativeDlls()
-    {
-        try
-        {
-            string exeDir = AppContext.BaseDirectory;
-            var asm = Assembly.GetExecutingAssembly();
-
-            foreach (var resourceName in asm.GetManifestResourceNames())
-            {
-                if (!resourceName.EndsWith(".dll", StringComparison.OrdinalIgnoreCase))
-                    continue;
-                string fileName = resourceName.Substring(resourceName.LastIndexOf('.', resourceName.Length - 5) + 1);
-                string destPath = Path.Combine(exeDir, fileName);
-
-                if (File.Exists(destPath)) continue; 
-
-                using var stream = asm.GetManifestResourceStream(resourceName);
-                if (stream == null) continue;
-
-                using var fileStream = File.Create(destPath);
-                stream.CopyTo(fileStream);
-            }
-        }
-        catch
-        {
-
-        }
     }
 
     protected override void OnExit(ExitEventArgs e)
